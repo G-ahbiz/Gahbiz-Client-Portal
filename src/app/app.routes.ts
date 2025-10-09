626f879f9f5bee99fb0cacb76f4962a10950f727
abdelawal <abdelawal89@gmail.com>
2025-10-09 17:36:13 +0300

    feat: update landing page and footer with new routes and components
    - Changed the title in index.html from 'GahbizClientPortal' to 'Servabest'.
    - Added new routes for Terms and Privacy pages in app.routes.ts.
    - Included 'Our Services' component in landingpage.html for enhanced service display.
    - Updated hero component images to use dynamic sources based on language in hero.html.
    - Improved footer links to navigate to the new Terms and Privacy pages, with updated translations in i18n files.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { NoAuthGuard } from '@core/guards/no-auth.guard';
 import { Landingpage } from '@features/landingpage/landingpage';
 import { Layout } from '@features/layout/layout';
 import { NotFound } from '@shared/components/not-found/not-found';
+import { Terms } from '@shared/components/terms/terms';
+import { Privacy } from '@shared/components/privacy/privacy';
 
 export const routes: Routes = [
   {
     path: 'home', component: Layout, children: [
       { path: '', component: Landingpage },
+      { path: 'terms-and-conditions', component: Terms },
+      { path: 'privacy-policy', component: Privacy },
       { path: '**', redirectTo: 'Error404', pathMatch: 'full' },
     ]
   },
@@ -29,6 +33,4 @@ export const routes: Routes = [
     canActivateChild: [NoAuthGuard],
     loadChildren: () => import('./features/auth/auth.routing').then((m) => m.AUTH_ROUTES),
   },
-  // // Add other routes here
-  // { path: '**', redirectTo: '' }, // Wildcard route for 404
 ];
--- src/app/features/landingpage/landingpage.ts
+++ src/app/features/landingpage/landingpage.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from "@features/landingpage/components/hero/hero";
+import { OurServices } from "./components/our-services/our-services";
 
 @Component({
   selector: 'app-landingpage',
-  imports: [Hero],
+  imports: [Hero, OurServices],
   templateUrl: './landingpage.html',
   styleUrl: './landingpage.scss'
 })
