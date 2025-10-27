98f4eb0dee5fd43a147f5240ae6d386c0fda286e
abdelawal <abdelawal89@gmail.com>
2025-10-28 02:14:32 +0300

    feat: enhance cart summary component with language support
    - Updated the cart summary component to implement language support, allowing for dynamic language changes based on user preferences.
    - Adjusted the title font size for improved readability.
    - Added logic to manage language direction and state based on the selected language.

--- src/app/features/cart/cart-summary/cart-summary.ts
+++ src/app/features/cart/cart-summary/cart-summary.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TranslateModule, TranslateService, LangChangeEvent } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cart-summary',
@@ -8,6 +8,48 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './cart-summary.html',
   styleUrl: './cart-summary.scss'
 })
-export class CartSummary {
+export class CartSummary implements OnInit {
+
+  // Language
+  isArabic: boolean = false;
+  isEnglish: boolean = false;
+  isSpanish: boolean = false;
+
+  @Input() cartItems: any[] | undefined;
+
+  constructor(private translateService: TranslateService) { }
+
+  ngOnInit() {
+    this.initializeTranslation();
+  }
+
+  // Initialize translation
+  private initializeTranslation() {
+    // Set default language if not already set
+    if (!localStorage.getItem('servabest-language')) {
+      localStorage.setItem('servabest-language', 'en');
+    }
+
+    // Get saved language and set it
+    const savedLang = localStorage.getItem('servabest-language') || 'en';
+    this.translateService.setDefaultLang('en');
+    this.translateService.use(savedLang);
+    if (savedLang === 'en' || savedLang === 'sp') {
+      document.documentElement.style.direction = 'ltr';
+    } else if (savedLang === 'ar') {
+      document.documentElement.style.direction = 'rtl';
+    }
+
+    // Set initial language state
+    this.isArabic = savedLang === 'ar';
+    this.isEnglish = savedLang === 'en';
+    this.isSpanish = savedLang === 'sp';
+    // Subscribe to language changes
+    this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.isArabic = event.lang === 'ar';
+      this.isEnglish = event.lang === 'en';
+      this.isSpanish = event.lang === 'sp';
+    });
+  }
 
 }
